482d2f087356f29adcd41001580cb129ff18feec
juancmorenoh <camilo.mro@gmail.com>
2024-11-04 12:09:37 +0000

    Add solution for number of distinct avarages

---
+++ Arrays/2465-number_of_distinct_avarages.py
@@ -0,0 +1,62 @@
+'''
+Problem: Number of distinct avarages
+
+Difficulty: Easy
+
+Topic: Arrays
+
+Description: You are given a 0-indexed integer array nums of even length.
+
+As long as nums is not empty, you must repetitively:
+
+Find the minimum number in nums and remove it.
+Find the maximum number in nums and remove it.
+Calculate the average of the two removed numbers.
+The average of two numbers a and b is (a + b) / 2.
+
+For example, the average of 2 and 3 is (2 + 3) / 2 = 2.5.
+Return the number of distinct averages calculated using the above process.
+
+Note that when there is a tie for a minimum or maximum number, any can be removed.
+
+ 
+
+Example 1:
+
+Input: nums = [4,1,4,0,3,5]
+Output: 2
+Explanation:
+1. Remove 0 and 5, and the average is (0 + 5) / 2 = 2.5. Now, nums = [4,1,4,3].
+2. Remove 1 and 4. The average is (1 + 4) / 2 = 2.5, and nums = [4,3].
+3. Remove 3 and 4, and the average is (3 + 4) / 2 = 3.5.
+Since there are 2 distinct numbers among 2.5, 2.5, and 3.5, we return 2.
+Example 2:
+
+Input: nums = [1,100]
+Output: 1
+Explanation:
+There is only one average to be calculated after removing 1 and 100, so we return 1.
+'''
+
+def distinctAvarages(nums):
+    distinct_avarage_set = set()
+        
+    while len(nums) > 1:
+        max = 0
+        min = 100
+        for i in range(len(nums)): #decided to manually loop to find max and min value
+            if nums[i] > max:
+                max = nums[i]
+            if nums[i] < min:
+                min = nums[i]
+
+        nums.remove(max) #remove highest int
+        nums.remove(min) #remove min int
+        distinct_avarage_set.add((min + max) / 2) #add the avarage to new set
+    return len(distinct_avarage_set)
+
+nums = [4,1,4,0,3,5]
+
+print(distinctAvarages(nums))
+
+
